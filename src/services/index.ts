6c6ec2d7641034a7c7b6aa51ed3ffdddb393a41a
凉数中 <2791389901@qq.com>
2026-08-03 00:29:09 +0800

    更新 index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,5 @@ export { UserService } from './user.service';
 export { PostService } from './post.service';
 export { CommentService } from './comment.service';
 export { TagService } from './tag.service';
+export { ProfileService } from './profile.service';   // 新增
+export { MessageService } from './message.service';   // 新增
